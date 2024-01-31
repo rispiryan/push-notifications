84b4f7b0bf88f2c6bb31c0ec6e39d4f035f28bfa
Razmik Ispiryan <razmikispiryan@Razmiks-MacBook-Pro.local>
2024-01-31 12:29:20 +0400

    fixed code

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import {sendToVercelAnalytics} from "./vitals";
 import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 serviceWorkerRegistration.register();
