723c86bbda4ca6779766c89426e308db58344b97
Razmik Ispiryan <razmikispiryan@Razmiks-MacBook-Pro.local>
2024-02-01 00:39:15 +0400

    feat: test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,11 +30,12 @@
 // });
 
 self.addEventListener('push', event => {
-    console.log('push', event)
+    const json = event?.data?.json();
+    console.log('push', event?.data?.json()?.notification)
 
     const options = {
-        body: event?.data?.text?.(),
+        body: json.notification.body,
     };
 
-    event.waitUntil(self.registration.showNotification('Your App Name', options));
+    event.waitUntil(self.registration.showNotification(json.notification.title, options));
 });
