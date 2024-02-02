8bb7668937950c68681b03515a074250e1942719
Razmik Ispiryan <razmikispiryan@Razmiks-MacBook-Pro.local>
2024-02-02 14:49:40 +0400

    feat: added url handler

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     return (
         <div className="App">
             <button onClick={requestNotificationPermission}>
-                requestNotificationPermission
+                requestNotificationPermissions
             </button>
 
             <p className='token'>
